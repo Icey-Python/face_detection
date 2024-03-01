87218ada71ed710c1ffdb1c0a8ad663d22a09083
Icey-Python <ndungusamkelly5@gmail.com>
2024-03-02 00:15:01 +0300

    Added(core) code to detect face from an image using opencv

---
+++ app.py
@@ -0,0 +1,36 @@
+import matplotlib.pyplot as plt
+import cv2 
+import subprocess
+"""
+In this file i will try to use open cv to check for faces in images 
+
+later on i will try to make cropped photos based on photo position to meet the given passport standard of 600px by 600px
+"""
+
+#should take in the image as path
+def detect_face(image:str):
+    img = cv2.imread(image)
+    #convert to grayscale for better detection 
+    gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    #load the detector/classifier 
+    face_detector = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+ 
+    face = face_detector.detectMultiScale(
+    gray_img , scaleFactor=1.1, minNeighbors=5, minSize=(100, 100))    
+
+    #display selection
+    for (x, y, w, h) in face:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 4)
+
+    #display
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    plt.figure(figsize=(20,10))
+    plt.imsave("./images/detections/detection_test.jpg",img_rgb)
+    subprocess.call(["xdg-open","./images/detections/detection_test.jpg"])
+    plt.axis('off')
+    
+image_path = "./images/potriat_test.jpg"
+detect_face(image_path)
